ebcf42d502a58a662a550094309e5e195d5ba48b
Amina <amyna2001@hotmail.com>
2025-03-09 15:38:04 +0100

    Ažurirana glavna grana: izmijenjena količina proizvoda i obrisani dijelovi koda

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ manager = ProductManager()
 
 # Dodavanje proizvoda
 product1 = Product("Laptop", 1500, 5)
-product2 = Product("Telefon", 800, 10)
-product3 = Product("Televizor", 1000, 7)
+product2 = Product("Telefon", 800, 9)
+product3 = Product("Televizor", 1000, 3)
 
 manager.add_product(product1)
 manager.add_product(product2)
 manager.add_product(product3)
-
-# Prikaz proizvoda i ukupne vrijednosti
-manager.display_products()
-print(f"Ukupna vrijednost inventara: {manager.total_inventory_value()} KM")

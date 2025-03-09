0219ede06211b8b6104cdc2e80dba9110e95cdeb
Amina <amyna2001@hotmail.com>
2025-03-09 15:28:33 +0100

    Dodana klasa Cart za upravljanje korpom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product_manager import ProductManager
 from product import Product
+from cart import Cart
 
 manager = ProductManager()
 
@@ -11,6 +12,14 @@ product3 = Product("Televizor", 1000, 7)
 manager.add_product(product1)
 manager.add_product(product2)
 manager.add_product(product3)
+
+cart = Cart()
+cart.add_to_cart(product1)
+cart.add_to_cart(product2)
+cart.add_to_cart(product3)
+
+cart.display_cart()
+print(f"Ukupna vrijednost korpe: {cart.calculate_total()} KM")
 
 # Prikaz proizvoda i ukupne vrijednosti
 manager.display_products()
